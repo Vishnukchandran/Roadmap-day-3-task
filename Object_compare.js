e71a0db15732e52d5734649f3ef16b929a0c90e8
Vishnukchandran <vishnuvkc@gmail.com>
2023-12-28 13:32:06 +0530

    some changes

--- Object_compare.js
+++ Object_compare.js
@@ -1,13 +1,18 @@
-let obj1 = {name:"Person 1", age:5};
-let obj2 = { age:5, name:"Person 1"};
+let obj1 = { name: "Person 1", age: 5 };
+let obj2 = { age: 5, name: "Person 1" };
+let flag = true;
 
 for(let key in obj1){
-    if(obj1[key] == obj2[key]){
-        console.log("obj1 and obj2 are equal");
+    if(obj1[key] !== obj2[key]){
+        flag = false;
+        break;
+}
+}
+
+if(flag){
+     console.log("obj1 and obj2 are equal");
     }
    else{
         console.log("obj1 and obj2 are not equal");
 
     }
-
-}
